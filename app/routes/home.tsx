28e8cba956466ec83cd376e9ef2eb816ca69be9e
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-03-09 16:44:22 +0530

    BasicSetup Done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,4 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +8,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <h1 className="text-3xl text-indigo-700 font-extrabold">Welcome to React Router!</h1>
 }
