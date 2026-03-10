22cc5399902a4786b456747cb464dece98c328ff
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-03-10 21:41:25 +0530

    Navbar 2

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { Box } from "lucide-react"
 
 const Navbar = () => {
+  const handleAuthClick = async () => {}
   return (
-    <div>Navbar</div>
+    <header className='navbar'>
+      <nav className='inner'>
+        <div className='left'>
+          <div className='brand'>
+            <Box className="logo" />
+            <span className="name">
+              Archify
+            </span>
+          </div>
+
+          <ul className="links"> 
+            <a href="#">Products</a>
+            <a href="#">Pricing</a>
+            <a href="#">Community</a>
+            <a href="#">Enterprise</a>
+          </ul>
+        </div>
+
+        <div className="actions">
+          <button onClick={handleAuthClick} className="login">Log In</button>
+        </div>
+      </nav>
+    </header>
   )
 }
 
