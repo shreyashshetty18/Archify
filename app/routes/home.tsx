e3501c9e50bf08c91aa3ee28ea2a7f1b1ad8c6da
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-03-28 15:37:45 +0530

    HomePage 3

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -21,7 +21,6 @@ export default function Home() {
       </div>
       <h1>Build Beautiful Spaces at the speed of thought with Archify</h1>
       <p className="subtitle">Archify is a AI-First Design environment that helps you visualize, render, and ship architectural projects faster.</p>
-      <Button variant="primary" size="lg">Get Started - It's Free!</Button>
     </section>
   </div>);
 }
