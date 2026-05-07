fa62acae3eef5e376facb2832692ead991f2e7cd
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-05-07 13:20:13 +0530

    Upload Sec 2

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useOutletContext } from 'react-router';
 
 const Upload = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const {isSignedIn} = useOutletContext<AuthContext>();
   return (
-    <div>Upload</div>
+    <div className='upload'>
+    </div>
   )
 }
 
